ad257ece8a66a552b44b39727df66ef45c6279a3
Jerome <jeromelesne@gmail.com>
2024-01-30 16:38:40 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>PHP CRUD</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="./assets/css/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <header>
+            <h1>PHP CRUD</h1>
+        </header>
+    </body>
+</html>
