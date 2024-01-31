4b44a494023bda5f7b70d96a377ff4f01e19b7b5
Jerome <jeromelesne@gmail.com>
2024-01-31 10:19:33 +0100

    setup db connection and form

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php
+$servername = 'db:3306';
+$username = 'db';
+$password = 'db';
+
+function dbConnect($connect, $servername, $username, $password)
+{
+    if($connect) {
+        try {
+            $db = new PDO("mysql:host=$servername; dbname=db", $username, $password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "db connection OK";
+            return $db;
+        } catch (PDOException $e) {
+            echo "Error : " . $e -> getMessage();
+            return $db;
+        }
+    } else {
+        $db = null;
+        return $db;
+    }
+}
+
+// function createTable()
+// {
+//
+// }
+
 ?>
 
 <!doctype html>
@@ -13,6 +40,30 @@
     <body>
         <header>
             <h1>PHP CRUD</h1>
+            <?php
+            $db = dbConnect(true, $servername, $username, $password);
+?>
         </header>
+        <main>
+            <div>
+                <form method="POST">
+                    <div>
+                        <label for="firstName">First Name</label>
+                        <input id="firstName" type="text" name="firstName" value="">
+                    </div>
+                    <div>
+                        <label for="name">Name</label>
+                        <input id="name" type="text" name="name" value="">
+                    </div>
+                    <div>
+                        <label for="mail">Email</label>
+                        <input id="mail" type="text" name="mail" value="">
+                    </div>
+                    <div>
+                        <input id="subBtn" type="submit" name="subBtn" value="Submit">
+                    </div>
+                </form>
+            </div>
+        </main>
     </body>
 </html>
